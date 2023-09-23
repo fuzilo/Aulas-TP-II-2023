bc12b34c1611c2e9af03c043486d399c5d1addcf
fuzilo <fuzilo@hotmail.com>
2023-09-22 22:51:45 -0300

    Update Sistema_Notas.py

---
+++ Arquivos de aula/ExercicioParticipação22.Setembro/Sistema_Notas.py
@@ -0,0 +1,13 @@
+#Faça um programa para corrigir uma prova com 10 questões de
+#multipla escolha ( ´ a, b, c, d ou e), em uma turma com 3 alunos.
+#Cada questão vale 1 ponto.
+#Leia o gabarito, e para cada aluno leia sua matricula (número inteiro) e
+#suas respostas.
+#Calcule e escreva: Para cada aluno, escreva sua matrícula, suas
+#respostas, e sua nota. O percentual de aprovação, média 7.0.*/
+
+tamanho = int(input("Digite q quantidade de alunos a serem avaliados\n"))
+vetor=[]
+#fazer iteração pra criar gabarito
+gabarito=[a,a,a,a,a,a,a,a,a,a]
+
